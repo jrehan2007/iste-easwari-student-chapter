bf5536f1c5256c1f7302f802bae7258170a45132
Rehan J <jrehan2007@gmail.com>
2026-09-17 10:16:41 +0530

    Update intro video

--- src/components/IntroHero.tsx
+++ src/components/IntroHero.tsx
@@ -59,7 +59,7 @@ export default function IntroHero() {
         {playing && (
           <motion.div
             key="intro"
-            className="fixed inset-0 z-50 flex items-center justify-center bg-black"
+            className="fixed inset-0 z-50 flex items-center justify-center bg-[#000A0B]"
             initial={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.9 }}
           >
             <video
